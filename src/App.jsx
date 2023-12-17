00b4982100501876da491c40c5ba2fa543c6a986
unknown <vornholt.leon@gmail.com>
2023-12-17 14:40:27 +0100

    add tailwindcss styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,18 @@ function App() {
 
   return (
     <>
-      <h1>Tool for hash functions</h1>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <CreateHash text={text} />
+      <body className="bg-zinc-700 h-screen text-zinc-50 flex flex-col">
+        <h1 className="text-2xl mx-auto my-8 tracking-wider">
+          Tool for hash functions
+        </h1>
+        <input
+          type="text"
+          value={text}
+          className="text-zinc-900 w-1/4 mx-auto"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <CreateHash text={text} />
+      </body>
     </>
   );
 }
--- src/CreateHash.jsx
+++ src/CreateHash.jsx
@@ -3,5 +3,9 @@ import hash from "hash.js";
 export default function CreateHash({ text }) {
   const test = hash.sha256().update(text).digest("hex");
 
-  return <h3>{test}</h3>;
+  return (
+    <div className="mx-auto my-9">
+      <h3>{test}</h3>
+    </div>
+  );
 }
